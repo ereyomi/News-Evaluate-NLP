d6b2e467ede5b1f1bb5ffe530f7a4e3229703c0c
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2020-06-19 22:36:58 +0100

    refactor server.js

--- src/server/index.js
+++ src/server/index.js
@@ -2,31 +2,63 @@ const path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
-//aylien text api
-const aylien = require( "aylien-news-api" );
+
+
+
 
 //dot env
 const dotenv = require('dotenv');
 dotenv.config();
 
-//key from env file
-console.log(`Your API key is ${process.env.API_KEY}`);
+//aylien text api
+const aylien = require( "aylien_textapi" );
+//aylien-news-api
+const textapi = new aylien({
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY
+} ); 
+
+textapi.sentiment({
+  'text': 'John is a very good football player!'
+}, function(error, response) {
+  if (error === null) {
+    console.log(response);
+  }
+} );
+
+
+
+
 
 const app = express()
+
+/* Dependences */
+const bodyParser = require( 'body-parser' );
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require( 'cors' );
+app.use( cors() );
+
 
 app.use(express.static('dist'))
 
 console.log(__dirname)
 
+
+
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    //res.sendFile(path.resolve('src/client/views/index.html'))
-    res.sendFile(path.resolve('./../../src/client/views/index.html'))
+    //res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
